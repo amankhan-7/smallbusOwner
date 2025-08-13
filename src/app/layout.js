93e068d5025da17cc644fa7770c31d1f314bb222
amankhan-7 <139583696+amankhan-7@users.noreply.github.com>
2025-08-13 18:43:50 +0530

    build ready

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxWrapper from "@/components/wrapper/redux-wrapper";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ReduxWrapper>{children}</ReduxWrapper>
+        <ReduxWrapper>
+          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+        </ReduxWrapper>
       </body>
     </html>
   );
